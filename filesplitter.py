38f2a32ba2812eca92e0c892c04d248945c1af01
prazman <olivierpraseuth@gmail.com>
2018-04-09 15:53:48 +0200

    linted the file to correct python

--- filesplitter.py
+++ filesplitter.py
@@ -1,42 +1,29 @@
 #!/usr/bin/env python
-import sys  
+import sys
 import os
 
-def main():  
-    filepath = sys.argv[1]
 
+def main():
+    filepath = sys.argv[1]
     if not os.path.isfile(filepath):
         print("File path {} does not exist. Exiting...".format(filepath))
         sys.exit()
 
     files = {}
     with open(filepath) as fp:
-        cnt = 0
         for line in fp:
-            write_line_to_files(line,files)
+            write_line_to_files(line, files)
 
 
-def write_line_to_files(line,files):
-    l = len(line)
-    if(l in files):
-        files[l].write(line)
+def write_line_to_files(line, files):
+    line_length = len(line)
+    if(line_length in files):
+        files[line_length].write(line)
     else:
-        name = './split/file_'+str(l)
-        files[l]=open(name,'w')
-        files[l].write(line)
-#  sorted_lengths = order_line_length_array(line_length_array, desc=True)
-#   print("Length {}".format(sorted_lengths))
+        name = './split/file_' + str(line_length)
+        files[line_length] = open(name, 'w')
+        files[line_length].write(line)
 
-# def order_line_length_array(line_length_array, desc=False):  
-#    lengths = [(length, cnt) for length, cnt in line_length_array.items()]
-#    return sorted(lengths, key=lambda x: x[1], reverse=desc)
 
-# def record_line_length(line, line_length_array):  
-#   l = len(line)
-#   if l in line_length_array:
-#     line_length_array[l]+=1
-#   else:
-#     line_length_array[l]=1
-
-if __name__ == '__main__':  
-   main()
+if __name__ == '__main__':
+    main()
